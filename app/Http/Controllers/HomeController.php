df7c14265eb4bc66bd33c555eee8d60b5befc930
Omar Belghali <omar.belghali@gmail.com>
2022-08-05 22:37:26 +0200

    first version of the code

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+
+    public function index()
+    {
+        return view('user.home');
+    }
+
+
+    public function redirect()
+    {
+       $usertype = Auth::user()->usertype;
+
+       if($usertype == '1')
+       {
+        return view('admin.home');
+       }
+       else
+       {
+        return view('home');
+       }
+    }
+}
